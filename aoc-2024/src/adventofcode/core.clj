fed4d95d42d68505d43672f7778fe0de0c99069c
Angelo Rosenfelder <angelo@rosenfelder.ch>
2024-12-02 00:23:42 +0100

    Solution day01_2
    Including tests, yay!🍾

--- aoc-2024/src/adventofcode/core.clj
+++ aoc-2024/src/adventofcode/core.clj
@@ -6,9 +6,9 @@
   "Reads a file with two numeric columns and returns a map with two vectors: :col1 and :col2."
   (with-open [rdr (io/reader (io/resource file-path))]
     (let [lines (line-seq rdr)
-          data  (map #(mapv (fn [location] (Integer/parseInt location)) (str/split % #"\s+")) lines)
-          locations1  (mapv first data)
-          locations2  (mapv second data)]
+          data (map #(mapv (fn [location] (Integer/parseInt location)) (str/split % #"\s+")) lines)
+          locations1 (mapv first data)
+          locations2 (mapv second data)]
       {:locations1 locations1 :locations2 locations2})))
 
 (defn sort-locations [columns]
@@ -24,8 +24,19 @@
         differences (map #(Math/abs (- %1 %2)) loc1 loc2)]
     (reduce + differences)))
 
-(defn solve [file-path]
-  "Reads a file and returns a map with sorted :locations1 and :locations2."
+(defn count-occurrences [locations n]
+  "Counts how many times the number 'n' occurs in the vector 'locations'."
+  (count (filter #(= % n) locations)))
+
+(defn sum-of-occurrences [columns]
+  "Iterates over :locations1 and counts how many times each item occurs in :locations2, then sums the results."
+  (let [locations1 (get columns :locations1)
+        locations2 (get columns :locations2)]
+    (reduce + (map #(let [occurrences (count-occurrences locations2 %)]
+                      (* occurrences %)) locations1))))
+
+(defn solve-01-1 [file-path]
+  "Solves day01 part 1."
   (-> file-path
       read-columns
       sort-locations
@@ -33,5 +44,16 @@
 
 ;REPL command
 ;(require 'adventofcode.core :reload)
-;(def sorted-data (solve "day01/example.txt"))
-;(println sorted-data)
+;(def solution (solve-01-1 "day01/input.txt"))
+;(println solution)
+
+(defn solve-01-2 [file-path]
+  "Solves day01 part 2."
+  (-> file-path
+      read-columns
+      sum-of-occurrences))
+
+;REPL command
+;(require 'adventofcode.core :reload)
+;(def solution (solve-01-2 "day01/input.txt"))
+;(println solution)
--- aoc-2024/test/adventofcode/core_test.clj
+++ aoc-2024/test/adventofcode/core_test.clj
@@ -2,6 +2,18 @@
   (:require [clojure.test :refer :all]
             [adventofcode.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest day01-1-test
+  (testing "Test day01-1."
+    ;; Prepare test data
+    (let [file-path "resources/day01/example.txt"]
+
+      ;; Test
+      (is (= 11 (solve-01-1 "day01/example.txt"))))))
+
+(deftest day01-2-test
+  (testing "Test day01-2."
+    ;; Prepare test data
+    (let [file-path "resources/day01/example.txt"]
+
+      ;; Test
+      (is (= 31 (solve-01-2 "day01/example.txt"))))))
